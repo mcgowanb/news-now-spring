31a98e987fb5be5d70594f47488deb056e1a0707
Brian McGowan <mcgowan.b@gmail.com>
2018-07-19 00:18:52 +0100

    initial commit

---
+++ src/main/java/com/mcgowan/newsnow/NewsnowApplication.java
@@ -0,0 +1,12 @@
+package com.mcgowan.newsnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsnowApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mcgowan/newsnow/NewsnowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mcgowan.newsnow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsnowApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
