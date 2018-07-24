4d205c289efb3379a93e96ef6b2202a0d5812ac9
Brian McGowan <mcgowan.b@gmail.com>
2018-07-25 00:13:42 +0100

    £checking duplicates

--- src/main/java/com/mcgowan/newsnow/service/impl/NewsNow.java
+++ src/main/java/com/mcgowan/newsnow/service/impl/NewsNow.java
@@ -6,8 +6,9 @@ import com.mcgowan.newsnow.service.IWebPageLoader;
 import lombok.extern.log4j.Log4j2;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
+import twitter4j.ResponseList;
+import twitter4j.Status;
 import twitter4j.TwitterException;
 
 import javax.inject.Inject;
@@ -23,6 +24,8 @@ public class NewsNow implements INewsNow {
 
     private final Twitter twitter;
 
+    private ResponseList<Status> currentTweets;
+
     @Inject
     public NewsNow(IWebPageLoader webPageLoader, Twitter twitter) {
         this.webPageLoader = webPageLoader;
@@ -31,17 +34,30 @@ public class NewsNow implements INewsNow {
 
     @Override
     public void process() throws IOException, TwitterException {
+        currentTweets = twitter.getLatestTweets();
 
         Document doc = webPageLoader.getWebPage("http://www.newsnow.co.uk/h/World+News/Europe/Western/Republic+of+Ireland");
-        Elements links = doc.getElementsByClass("hll");
-//        log.info(links);
-        List<Headline> headlines = links.stream().map(this::createHeadline).collect(Collectors.toList());
+        List<Element> links = doc.getElementsByClass("hll").stream().limit(20).collect(Collectors.toList());
+        log.info(links);
+        List<Headline> headlinesToTweet = links.stream()
+                .filter(this::isDuplicateTweet)
+                .map(this::createHeadline)
+                .collect(Collectors.toList());
 
-//        Headline h = createHeadline(links.get(0));
-        twitter.publish(headlines);
-        log.info("====================== HEADLINES ==========================");
-        log.info(headlines);
+        if (!headlinesToTweet.isEmpty())
+            twitter.publish(headlinesToTweet);
+        else
+            log.info("No unique headlines to tweet. NFA");
+    }
 
+    public boolean isDuplicateTweet(Element e) {
+        String title = e.text().trim();
+        List<Status> matches = currentTweets.stream().filter(ct -> {
+            String existingTitle = ct.getText().split("https:")[0].trim();
+            return existingTitle.equals(title);
+        }).collect(Collectors.toList());
+
+        return !matches.isEmpty();
     }
 
     private Headline createHeadline(Element element) {
--- src/main/java/com/mcgowan/newsnow/service/impl/Twitter.java
+++ src/main/java/com/mcgowan/newsnow/service/impl/Twitter.java
@@ -4,6 +4,7 @@ import com.mcgowan.newsnow.model.Headline;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -28,13 +29,24 @@ public class Twitter {
     }
 
     public void publish(List<Headline> headlines) throws TwitterException {
+        log.info("Publishing {} tweet(s)", headlines.size());
         for (Headline h : headlines) {
-            publish(h);
-//            try {
-//                Thread.sleep(5000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
+            try {
+                publish(h);
+            } catch (TwitterException ex) {
+                log.error("Tweet {} is a duplicate", h.getHeadline());
+                log.error(ex);
+            }
         }
     }
+
+    public void getTimeline() throws TwitterException {
+        ResponseList<Status> timeline = twitter.getHomeTimeline();
+        log.info(timeline);
+    }
+
+    public ResponseList<Status> getLatestTweets() throws TwitterException {
+        return twitter.getUserTimeline();
+    }
+
 }
